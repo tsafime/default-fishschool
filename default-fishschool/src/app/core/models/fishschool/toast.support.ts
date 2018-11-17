f6f8dffcd9908e17e1b1d6873411ff7dce6aff00
Sharon Sofer <sharon.sofer@gmail.com>
2018-11-17 12:06:42 +0200

    default sort background color changed + remove toast messages scroll windows

--- default-fishschool/src/app/core/models/fishschool/toast.support.ts
+++ default-fishschool/src/app/core/models/fishschool/toast.support.ts
@@ -9,21 +9,17 @@ export class ToastSupport {
 
 	showSuccess(toast: ToastMessage) {
 		this.toastr.successToastr(toast.message, toast.type, ToastConfig);
-		window.scrollTo(0, 0);
 	}
 
 	showError(toast: ToastMessage) {
 		this.toastr.errorToastr(toast.message, toast.type, ToastConfig);
-		window.scrollTo(0, 0);
 	}
 
 	showWarning(toast: ToastMessage) {
 		this.toastr.warningToastr(toast.message, toast.type, ToastConfig);
-		window.scrollTo(0, 0);
 	}
 
 	showInfo(toast: ToastMessage) {
 		this.toastr.infoToastr(toast.message, toast.type, ToastConfig);
-		window.scrollTo(0, 0);
 	}
 }
